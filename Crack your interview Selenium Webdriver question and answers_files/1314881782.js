391d848f779673ab945eb18243bb704b09348177
Ritesh Mansukhani <riteshmansukhani@gmail.com>
2017-11-22 19:21:03 +0530

    Ritesh Adding More Files

---
+++ Crack your interview Selenium Webdriver question and answers_files/1314881782.js
@@ -0,0 +1,52 @@
+(function() {
+    var r,
+        formString = "<style type=\"text/css\">#af-form-1314881782 .af-body .af-textWrap{width:98%;display:block;float:none;}#af-form-1314881782 .af-body input.text, #af-form-1314881782 .af-body textarea{background-color:#FFFFFF;border-color:#D9D9D9;border-width:1px;border-style:solid;color:#C7C7C7;text-decoration:none;font-style:normal;font-weight:normal;font-size:24px;font-family:Verdana, sans-serif;}#af-form-1314881782 .af-body input.text:focus, #af-form-1314881782 .af-body textarea:focus{background-color:#FFFAD6;border-color:#030303;border-width:1px;border-style:solid;}#af-form-1314881782 .af-body label.previewLabel{display:block;float:none;text-align:left;width:auto;color:#CCCCCC;text-decoration:none;font-style:normal;font-weight:normal;font-size:24px;font-family:Helvetica, sans-serif;}#af-form-1314881782 .af-body{padding-bottom:15px;padding-top:15px;background-repeat:no-repeat;background-position:inherit;background-image:none;color:#CCCCCC;font-size:11px;font-family:Verdana, sans-serif;}#af-form-1314881782 .af-quirksMode{padding-right:60px;padding-left:60px;}#af-form-1314881782 .af-standards .af-element{padding-right:60px;padding-left:60px;}#af-form-1314881782 .buttonContainer input.submit{background-color:#0479C2;background-image:url(\"//aweber.com/images/forms/plain/buttons/grey.png\");color:#FFFFFF;text-decoration:none;font-style:normal;font-weight:normal;font-size:24px;font-family:Helvetica, sans-serif;}#af-form-1314881782 .buttonContainer input.submit{width:auto;}#af-form-1314881782 .buttonContainer{text-align:center;}#af-form-1314881782 button,#af-form-1314881782 input,#af-form-1314881782 submit,#af-form-1314881782 textarea,#af-form-1314881782 select,#af-form-1314881782 label,#af-form-1314881782 optgroup,#af-form-1314881782 option{float:none;position:static;margin:0;}#af-form-1314881782 div{margin:0;}#af-form-1314881782 form,#af-form-1314881782 textarea,.af-form-wrapper,.af-form-close-button,#af-form-1314881782 img{float:none;color:inherit;position:static;background-color:none;border:none;margin:0;padding:0;}#af-form-1314881782 input,#af-form-1314881782 button,#af-form-1314881782 textarea,#af-form-1314881782 select{font-size:100%;}#af-form-1314881782 select,#af-form-1314881782 label,#af-form-1314881782 optgroup,#af-form-1314881782 option{padding:0;}#af-form-1314881782,#af-form-1314881782 .quirksMode{width:100%;max-width:275px;}#af-form-1314881782.af-quirksMode{overflow-x:hidden;}#af-form-1314881782{background-color:#00698F;border-color:#632A63;border-width:2px;border-style:solid;}#af-form-1314881782{display:block;}#af-form-1314881782{overflow:hidden;}.af-body .af-textWrap{text-align:left;}.af-body input.image{border:none!important;}.af-body input.submit,.af-body input.image,.af-form .af-element input.button{float:none!important;}.af-body input.text{width:100%;float:none;padding:2px!important;}.af-body.af-standards input.submit{padding:4px 12px;}.af-clear{clear:both;}.af-element label{text-align:left;display:block;float:left;}.af-element{padding-bottom:5px;padding-top:5px;}.af-form-wrapper{text-indent:0;}.af-form{text-align:left;margin:auto;}.af-quirksMode .af-element{padding-left:0!important;padding-right:0!important;}.lbl-right .af-element label{text-align:right;}body {}</style><form method=\"post\" class=\"af-form-wrapper\" accept-charset=\"UTF-8\" action=\"https://www.aweber.com/scripts/addlead.pl\" target=\"_blank\" ><div style=\"display: none;\"><input type=\"hidden\" name=\"meta_web_form_id\" value=\"1314881782\" /><input type=\"hidden\" name=\"meta_split_id\" value=\"\" /><input type=\"hidden\" name=\"listname\" value=\"awlist4269687\" /><input type=\"hidden\" name=\"redirect\" value=\"http://www.aweber.com/thankyou-coi.htm?m=text\" id=\"redirect_09f6c4b4e7aea98a82bf7d3a78e02c93\" /><input type=\"hidden\" name=\"meta_adtracking\" value=\"My_Web_Form_3\" /><input type=\"hidden\" name=\"meta_message\" value=\"1\" /><input type=\"hidden\" name=\"meta_required\" value=\"name,email\" /><input type=\"hidden\" name=\"meta_tooltip\" value=\"\" /></div><div id=\"af-form-1314881782\" class=\"af-form\"><div id=\"af-body-1314881782\" class=\"af-body af-standards\"><div class=\"af-element\"><label class=\"previewLabel\" for=\"awf_field-82268132\">Name: </label><div class=\"af-textWrap\"><input id=\"awf_field-82268132\" type=\"text\" name=\"name\" class=\"text\" value=\"\"  onfocus=\" if (this.value == \'\') { this.value = \'\'; }\" onblur=\"if (this.value == \'\') { this.value=\'\';} \" tabindex=\"500\" /></div><div class=\"af-clear\"></div></div><div class=\"af-element\"><label class=\"previewLabel\" for=\"awf_field-82268133\">Email: </label><div class=\"af-textWrap\"><input class=\"text\" id=\"awf_field-82268133\" type=\"text\" name=\"email\" value=\"\" tabindex=\"501\" onfocus=\" if (this.value == \'\') { this.value = \'\'; }\" onblur=\"if (this.value == \'\') { this.value=\'\';} \" /></div><div class=\"af-clear\"></div></div><div class=\"af-element buttonContainer\"><input name=\"submit\" id=\"af-submit-image-1314881782\" type=\"image\" class=\"image\" style=\"background: none; max-width: 100%;\" alt=\"Submit Form\" src=\"//aweber.com/images/forms/plain/buttons/grey.png\" tabindex=\"502\" /><div class=\"af-clear\"></div></div></div></div></form><script type=\"text/javascript\">// Special handling for facebook iOS since it cannot open new windows(function() {    if (navigator.userAgent.indexOf(\'FBIOS\') !== -1 || navigator.userAgent.indexOf(\'Twitter for iPhone\') !== -1) {        document.getElementById(\'af-form-1314881782\').parentElement.removeAttribute(\'target\');    }})();</script><script type=\"text/javascript\">    <!--    (function() {        var IE = /*@cc_on!@*/false;        if (!IE) { return; }        if (document.compatMode && document.compatMode == \'BackCompat\') {            if (document.getElementById(\"af-form-1314881782\")) {                document.getElementById(\"af-form-1314881782\").className = \'af-form af-quirksMode\';            }            if (document.getElementById(\"af-body-1314881782\")) {                document.getElementById(\"af-body-1314881782\").className = \"af-body inline af-quirksMode\";            }            if (document.getElementById(\"af-header-1314881782\")) {                document.getElementById(\"af-header-1314881782\").className = \"af-header af-quirksMode\";            }            if (document.getElementById(\"af-footer-1314881782\")) {                document.getElementById(\"af-footer-1314881782\").className = \"af-footer af-quirksMode\";            }        }    })();    --></script>";
+    // Special handling for facebook iOS since it cannot open new windows
+    if (navigator.userAgent.indexOf('FBIOS') !== -1 || navigator.userAgent.indexOf('Twitter for iPhone') !== -1) {
+        formString = formString.replace(/ target=\"_blank\"/g, '');
+    }
+    try {
+        r = document.querySelectorAll('.AW-Form-1314881782');
+    }
+    catch (exception) {
+        // do nothing, the try catch is if the doctype is left as just HTML
+    }
+    if (r.length > 0) {
+        for(var i=0; i < r.length; i++){
+           var s = document.createElement('div');
+           s.innerHTML = formString;
+           r[i].insertBefore(s, r[i].firstChild);
+        }
+    } else {
+        document.write(formString);
+    }
+})();
+
+if (typeof window.awf_Form_=='undefined') window.awf_Form_ = new Object();
+awf_Form_.isPreview = false;
+awf_Form_.setCookie = function(name, value, expires, path, domain, secure) {
+    var curCookie = name + "=" + escape(value) +
+        ((expires) ? "; expires=" + expires.toGMTString() : "") +
+        ((path) ? "; path=" + path : "") +
+        ((domain) ? "; domain=" + domain : "") +
+        ((secure) ? "; secure" : "");
+    document.cookie = curCookie;
+}
+
+awf_Form_.getCookie = function(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    } else
+        begin += 2;
+    var end = document.cookie.indexOf(";", begin);
+    if (end == -1)
+        end = dc.length;
+    return unescape(dc.substring(begin + prefix.length, end));
+}
+
+var unique_track = new Image();
+        unique_track.src = "https://forms.aweber.com/form/displays.htm?id=jMyMLBwcjOwcTA==";
---
+++ Selenium Automation Framework Example  Selenium Easy_files/js_52D7zwxhC9leCxWwO11x93a0x_n0qh-GFTiQfLi9FlM.js
@@ -0,0 +1,43 @@
+(function($) {
+
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.3&appId=579264578881928";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+}(jQuery));
+;
+(function ($) {
+	Drupal.behaviors.backtotop = {
+		attach: function(context) {
+			var exist= jQuery('#backtotop').length;
+      if(exist == 0) {
+        $("body", context).once(function() {
+          $(this).append("<div id='backtotop'>"+Drupal.t(Drupal.settings.back_to_top.back_to_top_button_text)+"</div>");
+        });
+      }
+			$(window).scroll(function() {
+				if($(this).scrollTop() > Drupal.settings.back_to_top.back_to_top_button_trigger) {
+					$('#backtotop').fadeIn();	
+				} else {
+					$('#backtotop').fadeOut();
+				}
+			});
+
+      $('#backtotop', context).once(function() {
+			  $(this).click(function() {
+			    $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function() {
+            $('html, body').stop();
+          });
+          $('html,body').animate({ scrollTop: 0 }, 1200, 'easeOutQuart', function() {
+            $("html, body").unbind("scroll mousedown DOMMouseScroll mousewheel keyup");
+          });
+          return false;
+			  });
+			});
+		}
+	};
+})(jQuery);;
